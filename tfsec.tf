ffb7cc91c943ca75da4adf40b0826d55e9481418
Vlad Ionescu <Vlaaaaaaad@users.noreply.github.com>
2021-04-19 21:05:23 +0000

    ALL

--- tfsec.tf
+++ tfsec.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "example" {
 
 resource "aws_security_group_rule" "example_rule" {
   security_group_id = aws_security_group.example.id
-  description       = "Allow all traffic from Internet which will make tfsec much cranky"
+  description       = "Allow all traffic from Internet which will make tfsec so cranky"
 
   type      = "ingress"
   from_port = "0"
